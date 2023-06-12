ea7856474aa0500f089c5a7abef9357309898de0
drikadev <adriguti.adriana@hotmail.com>
2023-06-12 20:32:16 -0300

    correção JS

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const[assunto] = document.querySelector("textarea");
 const botaoEnviar = document.querySelector("#botao");
 
 function validarBotao(){
-    if(nome.value && email.value && assunto.value.length >= 10){
+    if(nome.value && email.value.indexOf("@") && assunto.value.length >= 10){
         botaoEnviar.classList.add("ativarCorBotao");
         botaoEnviar.removeAttribute("disabled");
     }else{
